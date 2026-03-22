7ff4f02ae9595d2856d40caea72beaca92518534
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2026-03-23 01:21:24 +0200

    feat(whyus):Add translastion

--- src/app/website/home/_components/why-us -section/content-side.tsx
+++ src/app/website/home/_components/why-us -section/content-side.tsx
@@ -1,59 +1,71 @@
 import { SectionSubTitle, SectionTitle } from "@/components/ui/section-head";
+import { useFormatter, useLocale, useTranslations } from "use-intl";
 
 export default function ContentSide() {
+  // Translations
+  const t = useTranslations("why-us-section");
+  const format = useFormatter();
+  const locale = useLocale();
+
   return (
     <div className="text-[#242424] dark:text-[#F3F3F4]">
-      <SectionTitle>why us</SectionTitle>
-      <SectionSubTitle>Why Us</SectionSubTitle>
+      {/* Section Head */}
+      <SectionTitle>{t("title")}</SectionTitle>
+      <SectionSubTitle>{t("title")}</SectionSubTitle>
 
+      {/* Section Description */}
       <h2 className="mt-6 w-146 font-bold text-[2.5rem] uppercase leading-12">
-        Elevate fitness with the <span className="text-[#FF4100]">best way</span> possible
+        {t.rich("desc-1", {
+          span: (chunck) => <span className="text-[#FF4100]">{chunck}</span>,
+        })}
       </h2>
-      <p className="mt-6 mb-16 w-146 text-lg">
-        We offer a fitness journey that's tailored to your goals, supported by professional trainers and a welcoming
-        community. Whether it's weight loss, strength building, or overall wellness, our proven methods.
-      </p>
+      <p className="mt-6 mb-16 w-146 text-lg">{t("desc-2")}</p>
+
+      {/* Section List */}
       <ul className="space-y-8 w-full max-w-[38.9rem]">
         <li className="flex items-center gap-10">
           <div>
             <span className="flex justify-center items-center bg-[#FF4100] border border-[#24242424] rounded-full w-14 h-14">
-              01
+              {locale === "en"
+                ? "01"
+                : format.number(1, {
+                    numberingSystem: locale === "ar" ? "arab" : "latn",
+                  })}
             </span>
           </div>
           <div className="space-y-2">
-            <h3 className="font-bold">personalized fitness plans</h3>
-            <p className="leading-relaxed">
-              We tailor every workout to fit your unique goals and fitness level ensuring that you make the most
-              progress.
-            </p>
+            <h3 className="font-bold capitalize">{t("li-1-title")}</h3>
+            <p className="leading-relaxed">{t("li-1-desc")}</p>
           </div>
         </li>
         <li className="flex items-center gap-10">
           <div>
             <span className="flex justify-center items-center bg-[#FF4100] border border-[#24242424] rounded-full w-14 h-14">
-              02
+              {locale === "en"
+                ? "02"
+                : format.number(2, {
+                    numberingSystem: locale === "ar" ? "arab" : "latn",
+                  })}
             </span>
           </div>
           <div className="space-y-2">
-            <h3 className="font-bold">personalized fitness plans</h3>
-            <p className="leading-relaxed">
-              We tailor every workout to fit your unique goals and fitness level ensuring that you make the most
-              progress.
-            </p>
+            <h3 className="font-bold capitalize">{t("li-2-title")}</h3>
+            <p className="leading-relaxed">{t("li-2-desc")}</p>
           </div>
         </li>
         <li className="flex items-center gap-10">
           <div>
             <span className="flex justify-center items-center bg-[#FF4100] border border-[#24242424] rounded-full w-14 h-14">
-              03
+              {locale === "en"
+                ? "03"
+                : format.number(3, {
+                    numberingSystem: locale === "ar" ? "arab" : "latn",
+                  })}
             </span>
           </div>
           <div className="space-y-2">
-            <h3 className="font-bold">personalized fitness plans</h3>
-            <p className="leading-relaxed">
-              We tailor every workout to fit your unique goals and fitness level ensuring that you make the most
-              progress.
-            </p>
+            <h3 className="font-bold capitalize">{t("li-3-title")}</h3>
+            <p className="leading-relaxed">{t("li-3-desc")}</p>
           </div>
         </li>
       </ul>
--- src/app/website/home/page.tsx
+++ src/app/website/home/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/layout/footer/footer";
 import MarqueeText from "@/components/layout/marquee/marquee-text";
-import { SectionTitle } from "@/components/ui/section-head";
 import { useTranslations } from "use-intl";
 import WhyUs from "./_components/why-us -section/why-us";
 
@@ -11,7 +10,7 @@ export default function HomePage() {
     <main className="bg-background">
       <h1>{t("hello", { name: "Ali" })}</h1>
       <p className="font-inter">{t("welcome")}</p>
-      <SectionTitle variant={"center"}>{t("welcome")}</SectionTitle>
+
       <WhyUs />
       <MarqueeText />
       <Footer />
