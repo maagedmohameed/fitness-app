c7c935a59534156f52235f5374f4f90c6ff4a6bc
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2026-03-23 00:04:23 +0200

    feat(meals): implement meals section.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,28 +4,24 @@ import { useTranslations } from "use-intl";
 export default function NotFound() {
   const t = useTranslations("notfound");
   return (
-    <main className="w-full h-dvh  m-auto flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center text-sm max-md:px-4 py-20">
-        <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-black/50 dark:from-white to-gray-500 bg-clip-text text-transparent">
+    <main className="flex flex-col justify-center items-center m-auto w-full h-dvh">
+      <div className="flex flex-col justify-center items-center max-md:px-4 py-20 text-sm">
+        <h1 className="bg-clip-text bg-linear-to-r from-black/50 dark:from-white to-gray-500 font-bold text-transparent text-4xl md:text-5xl">
           {t("title")}
         </h1>
-        <div className="h-px w-80 rounded bg-gradient-to-r from-gray-400 to-gray-800 my-5 md:my-7"></div>
-        <p className="md:text-xl text-gray-400 max-w-lg text-center">
-          {t("desc")}
-        </p>
+        <div className="bg-linear-to-r from-gray-400 to-gray-800 my-5 md:my-7 rounded w-80 h-px"></div>
+        <p className="max-w-lg text-gray-400 md:text-xl text-center">{t("desc")}</p>
         <Link
           to="/"
-          className="group flex items-center gap-1 bg-white hover:bg-gray-200 px-7 py-2.5 text-gray-800 rounded-full mt-10 font-medium active:scale-95 transition-all"
-        >
+          className="group flex items-center gap-1 bg-white hover:bg-gray-200 mt-10 px-7 py-2.5 rounded-full font-medium text-gray-800 active:scale-95 transition-all">
           {t("back")}
           <svg
-            className="group-hover:translate-x-0.5 transition"
+            className="transition group-hover:translate-x-0.5"
             width="22"
             height="22"
             viewBox="0 0 22 22"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+            xmlns="http://www.w3.org/2000/svg">
             <path
               d="M4.583 11h12.833m0 0L11 4.584M17.416 11 11 17.417"
               stroke="#1E1E1E"
---
+++ src/app/website/home/_components/meals-section/h-meals.tsx
@@ -0,0 +1,51 @@
+import MealCard from "@/components/shared/meal-card";
+import { SectionSubTitle, SectionTitle } from "@/components/ui/section-head";
+import { useTranslations } from "use-intl";
+
+const MEALS = [
+  {
+    title: "breakfast",
+    mealImage: "/assets/images/meal2.webp",
+  },
+  {
+    title: "lunch",
+    mealImage: "/assets/images/meal1.webp",
+  },
+  {
+    title: "dinner",
+    mealImage: "/assets/images/meal3.webp",
+  },
+];
+
+export default function Meals() {
+  // Translations
+  const t = useTranslations("h-meal-section");
+
+  return (
+    <section className="bg-[url(/assets/images/healthy-bg.webp)] bg-cover bg-center px-1 py-8 h-screen">
+      {/* Section Head */}
+      <SectionTitle variant={"center"} className="from-[#FFFFFF]/20 via-[#FFFFFF]/10 -mb-6">
+        {t("title")}
+      </SectionTitle>
+
+      {/* Section Content */}
+      <div className="flex flex-col items-center gap-8 bg-white/60 dark:bg-[#242424]/60 backdrop-blur-xl h-104">
+        {/* SubTitle */}
+        <SectionSubTitle variant={"center"}>{t("subTitle")}</SectionSubTitle>
+        {/* Description */}
+        <p className="w-[39.8rem] font-bold text-[#242424] dark:text-[#F3F3F4] text-4xl text-center uppercase leading-12">
+          {t.rich("desc", {
+            span: (chunk) => <span className="text-[#FF4100]">{chunk}</span>,
+          })}
+        </p>
+
+        {/* Section Cards */}
+        <div className="flex justify-center items-center gap-6 px-2.5">
+          {MEALS.map((meal, idx) => (
+            <MealCard key={idx} title={meal.title} mealImage={meal.mealImage} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
