1c05347244873f27857af4d6faa32c5a5d9f2061
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2026-03-24 08:36:47 +0200

    feat(meals):solve comments

--- src/app/website/home/page.tsx
+++ src/app/website/home/page.tsx
@@ -2,7 +2,6 @@ import { ErrorBoundary } from "react-error-boundary";
 import ErrorBoundaryFallback from "@/components/shared/error-boundary-fallback";
 import Footer from "@/components/layout/footer/footer";
 import MarqueeText from "@/components/layout/marquee/marquee-text";
-import { useTranslations } from "use-intl";
 import WhyUs from "./_components/why-us -section/why-us";
 import WorkoutSection from "@/app/website/home/_components/workout-section";
 import Meals from "./_components/meals-section/h-meals";
