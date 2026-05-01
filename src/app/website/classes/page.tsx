51919c6e7115a561f95b371cb6760f5f1ac0a1a1
Ahmed Alaa Khalaf <ahmed.khalaf1611@gmail.com>
2026-05-01 19:13:46 +0300

    fix(classes): add small padding to the classes page

--- src/app/website/classes/page.tsx
+++ src/app/website/classes/page.tsx
@@ -9,7 +9,7 @@ export default function ClassesPage() {
   return (
     <main className="bg-[url(/assets/images/workout-section-cover.webp)] bg-cover">
       {/* Section Head */}
-      <div className="bg-[#ffffff]/60 dark:bg-[#24242499] backdrop-blur-[5.375rem]">
+      <div className="bg-[#ffffff]/60 dark:bg-[#24242499] backdrop-blur-[5.375rem] py-12 sm:py-36">
         <SectionTitle
           variant={"center"}
           className="from-[#FFFFFF]/20 via-[#FFFFFF]/10 -mb-6"
